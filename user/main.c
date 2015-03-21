24b7d3511608439f440ca676853d7eca27ee570d
cposture <cposture@126.com>
2015-03-21 12:08:12 +0800

    v1.1 2015/3/21 12:07
    增加了方向余弦函数
    void evaluateDirectionCosine(int16_t accel[3],float a[3],float
    accel_res[3])

--- user/main.c
+++ user/main.c
@@ -97,13 +97,26 @@ void Send_Data(int16_t *Gyro,int16_t *Accel)
 		printf("%c",data_to_send[i]);
 }
 
+/* a[0]航向角 a[1]俯仰角 a[2]横滚角 */
+void evaluateDirectionCosine(int16_t accel[3],float a[3],float accel_res[3])
+{
+	float cos_a,cos_b,cos_c,sin_a,sin_b,sin_c;
 
+	cos_a =	cos(a[0]); cos_b = cos(a[1]); cos_c = cos(a[2]);
+	sin_a = sin(a[0]); sin_b = sin(a[1]); sin_c = sin(a[2]);  
+
+	accel_res[0] = (cos_c*cos_a-cos_b*sin_a*sin_c)*accel[0] + (cos_c*sin_a+cos_b*cos_a*sin_c)*accel[1] + ( sin_c*cos_b )*accel[2];
+	accel_res[1] = ( -sin_c*cos_a-cos_b*sin_a*cos_c )*accel[0] + ( -sin_c*sin_a + cos_b*cos_a*cos_c )*accel[1] + ( cos_c*sin_b )*accel[2];
+	accel_res[2] = ( sin_b*sin_c )*accel[0] + ( -sin_b*cos_c )*accel[1] + cos_b*accel[2];
+
+}
 
 int main(void)
 {
 	struct int_param_s int_param;
 	signed char gyro_orientation[9] = {0, -1, 0, 1,0, 0, 0, 0, 1};
 	int16_t gyro[3], accel[3];
+	float accel_res[3],gyro_res[3];
 	long quat[4];
   unsigned long timestamp; 
 	short sensors;
@@ -155,13 +168,14 @@ unsigned char more;
 //			 Pitch = asin(-2 * q[1] * q[3] + 2 * q[0]* q[2])* 57.3; // pitch
 //			 Roll = atan2(2 * q[2] * q[3] + 2 * q[0] * q[1], -2 * q[1] * q[1] - 2 * q[2]* q[2] + 1)* 57.3; // roll
 //			 Yaw = 	atan2(2*(q[1]*q[2] + q[0]*q[3]),q[0]*q[0]+q[1]*q[1]-q[2]*q[2]-q[3]*q[3]) * 57.3;		//感觉没有价值，注掉
-			Pitch = asin(-2 * q[1] * q[3] + 2 * q[0]* q[2]); // pitch
-			 Roll = atan2(2 * q[2] * q[3] + 2 * q[0] * q[1], -2 * q[1] * q[1] - 2 * q[2]* q[2] + 1); // roll
-			 Yaw = 	atan2(2*(q[1]*q[2] + q[0]*q[3]),q[0]*q[0]+q[1]*q[1]-q[2]*q[2]-q[3]*q[3]) ;		//感觉没有价值，注掉
+			gyro_res[1] = asin(-2 * q[1] * q[3] + 2 * q[0]* q[2]); // pitch
+			 gyro_res[2] = atan2(2 * q[2] * q[3] + 2 * q[0] * q[1], -2 * q[1] * q[1] - 2 * q[2]* q[2] + 1); // roll
+			 gyro_res[0] = 	atan2(2*(q[1]*q[2] + q[0]*q[3]),q[0]*q[0]+q[1]*q[1]-q[2]*q[2]-q[3]*q[3]) ;		//感觉没有价值，注掉
 //			printf("%f,%f,%f\n",Pitch,Roll, Yaw);
 			//printf("%3f,%3f,%3f\n",accel[0]/16384.0,accel[1]/16384.0, accel[2]/16384.0);
 			// printf("%3f,%3f,%3f\n",cos(1.57+Roll),cos(1.57+Pitch), cos(1.57+Yaw));
-			printf("%3f,%3f,%3f\n",accel[0]/16384.0+cos(1.57+Roll) ,accel[1]/16384.0+cos(1.57+Pitch), accel[2]/16384.0+cos(1.57+Yaw));
+			evaluateDirectionCosine(accel,gyro_res,accel_res);
+			printf("%3f,%3f,%3f\n",accel_res[0]/16384.0,accel_res[1]/16384.0, accel_res[2]/16384.0);
 //Data_Send_Status(Pitch,Roll,-Yaw,gyro,accel);
 //			delay_ms(10);
 		}
